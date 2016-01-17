0e655d1b055b26212e6d5e31d36250a353b20bf3
captn51 <captn-51@hotmail.fr>
2016-01-17 14:23:23 +0100

    Ajout de messages et de la longueur du serpent + respect de conventions

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from sys import exit
+from time import sleep
 from random import choice, randrange
 import pygame
 from pygame.locals import *
@@ -41,62 +42,67 @@ class Snake:
             randrange(self._INITIAL_LEN*self._PIECE_SIZE, i-self._INITIAL_LEN*self._PIECE_SIZE, self._PIECE_SIZE)
             for i in area
         ]
-        self.area = area
-        self.digestion = False
-        self.direction = choice(list(self.DIRECTIONS.values()))
-        self.body = [
-            [p_i - i*self._PIECE_SIZE*d for p_i, d in zip(pos_i, self.direction)]
+        self._area = area
+        self._digestion = False
+        self._direction = choice(list(self.DIRECTIONS.values()))
+        self._body = [
+            [p_i - i*self._PIECE_SIZE*d for p_i, d in zip(pos_i, self._direction)]
             for i in range(self._INITIAL_LEN)
         ]
 
-    def set_direcion(self, key_direction):
+    def set_direction(self, key_direction):
         """Change la direction du serpent.
         """
         # Pas possible de faire demi-tour
-        new_plus_old = [new + old for new, old in zip(self.DIRECTIONS[key_direction], self.direction)]
+        new_plus_old = [new + old for new, old in zip(self.DIRECTIONS[key_direction], self._direction)]
         if new_plus_old != [0, 0]:
-            self.direction = self.DIRECTIONS[key_direction]
+            self._direction = self.DIRECTIONS[key_direction]
 
     def move(self):
         """Déplace le serpent.
 
         Le serpent retourne de l'autre côté de la zone s'il en sort.
         """
-        if self.digestion:
-            self.body[1:] = self.body
-            self.digestion = False
+        if self._digestion:
+            self._body[1:] = self._body
+            self._digestion = False
         else:
-            self.body[1:] = self.body[:len(self.body)-1]
+            self._body[1:] = self._body[:len(self._body)-1]
 
-        self.body[0] = [b + self._PIECE_SIZE*d for b, d in zip(self.body[0], self.direction)]
+        self._body[0] = [b + self._PIECE_SIZE*d for b, d in zip(self._body[0], self._direction)]
 
         # Gestion aux bords de la zone
         for i in range(2):
-            if self.body[0][i] < 0:
-                self.body[0][i] = self.area[i] - self._PIECE_SIZE
-            elif self.body[0][i] > self.area[i]-self._PIECE_SIZE:
-                self.body[0][i] = 0
+            if self._body[0][i] < 0:
+                self._body[0][i] = self._area[i] - self._PIECE_SIZE
+            elif self._body[0][i] > self._area[i]-self._PIECE_SIZE:
+                self._body[0][i] = 0
 
     def move_on_itself(self):
         """Indique si le serpent se déplace sur lui-même ou non.
         """
-        return self.body[0] in self.body[1:]
+        return self._body[0] in self._body[1:]
 
     def eat(self, cube):
         """Indique si le serpent a mangé un cube ou non.
 
         Si le serpent a mangé un cube, il entre en phase de digestion.
         """
-        if cube.pos == self.body[0]:
-            self.digestion = True
+        if cube.pos == self._body[0]:
+            self._digestion = True
             return True
         else:
             return False
 
+    def __len__(self):
+        """Renvoie la longueur du serpent.
+        """
+        return len(self._body)
+
     def draw(self, screen, rgb_color):
         """Dessine un serpent dans une fenêtre avec une couleur donnée.
         """
-        for piece in self.body:
+        for piece in self._body:
             pygame.draw.rect(screen, DARK_GRAY, Rect(piece[0], piece[1], self._PIECE_SIZE, self._PIECE_SIZE))
 
 
@@ -113,6 +119,7 @@ class Cube:
 
         Le cube apparait à un endroit aléatoire de la zone.
         """
+        # TODO: property ?
         self.pos = [randrange(0, i, self._PIECE_SIZE) for i in area]
 
     def move(self, area):
@@ -131,6 +138,10 @@ class Cube:
 # ====
 
 
+print("Bienvenue dans le jeu du SNAKE !!")
+print("Attention !! Départ dans 2s !!")
+sleep(2)
+
 pygame.init()
 
 snake = Snake(WINDOWS_SIZE)
@@ -141,17 +152,19 @@ screen = pygame.display.set_mode(WINDOWS_SIZE)
 pygame.display.set_caption("Snake")
 snake.draw(screen, DARK_GRAY)
 cube.draw(screen, DARK_GRAY)
+pygame.display.flip()
+print("Longueur du serpent :", len(snake), end="")
 
 while True:
     my_clock.tick(FRAMERATE)
     for event in pygame.event.get():
-        # TODO: cas où 2 set_direcion se cumulent car appui trop rapide sur les touches
+        # TODO: cas où 2 set_direction se cumulent car appui trop rapide sur les touches
 
         if event.type == QUIT:
             exit()
 
         if event.type == KEYDOWN and event.key in snake.DIRECTIONS.keys():
-            snake.set_direcion(event.key)
+            snake.set_direction(event.key)
 
     snake.move()
     if snake.move_on_itself():
@@ -164,6 +177,11 @@ while True:
     snake.draw(screen, DARK_GRAY)
     cube.draw(screen, DARK_GRAY)
     pygame.display.flip()
+    print("\rLongueur du serpent :", len(snake), end="")
+
+print("")
+print("HA HA !! Tu t'es mangé toi-même !! La longueur atteinte est de", len(snake), "!!")
+input("Appuie sur ENTREE pour quitter...")
 
 pygame.quit()
 
